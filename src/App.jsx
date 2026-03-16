35e1a3e5f77f9a8ec6fa69d92da350e6525e6d82
Ahmed Doaya <ahmed.doaia10@gmail.com>
2026-03-16 04:17:37 +0200

    fix route import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
 import { createBrowserRouter, Route, RouterProvider } from 'react-router'
-import routes from './Route/Route'
+import routes from './Route/route'
 createBrowserRouter
 export default function App() {
   return (
